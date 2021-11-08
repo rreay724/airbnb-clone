5632a01546bc23640a4106dcccd81c8856aeb4c5
Bobby Reay <rreay724@gmail.com>
2021-11-08 12:46:26 -0500

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,10 @@ import { footerText } from "../footerText";
 function Footer() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 bg-gray-100 px-32 py-14 text-gray-600 gap-y-10">
-      {footerText.map((item) => (
+      {footerText.map(({ title, text }) => (
         <div className="space-y-4 text-xs text-gray-800">
-          <h5 className="font-bold">{item.title}</h5>
-          {item.text.map((text) => (
+          <h5 className="font-bold">{title}</h5>
+          {text.map((text) => (
             <p>{text}</p>
           ))}
         </div>
